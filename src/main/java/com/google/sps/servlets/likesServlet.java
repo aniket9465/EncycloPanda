2be3e426f9b7c6f2a3fe2921142a6e7449515193
VanyaBK <37258663+VanyaBK@users.noreply.github.com>
2020-08-12 12:06:08 +0530

    Create likesServlet

---
+++ src/main/java/com/google/sps/servlets/likesServlet.java
@@ -0,0 +1,98 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.data.Comment;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Manages number of likes for a comment from a user */
+@WebServlet("/likes")
+public class DataServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    /** If user is not logged in redirect to authentication */
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/authentication");
+      return;
+    }
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    
+    String userId_like = userService.getCurrentUser().getUserId();
+    long postId_like = request.getParameter("postId");
+
+    Entity likesEntity = new Entity("Likes");
+    likesEntity.setProperty("userId", userId_like);
+    likesEntity.setProperty("postId", postId_like);
+	
+    Query query_likes = new Query("Likes");
+    PreparedQuery results_likes = datastore.prepare(query_likes);
+	
+    /** Check for multiple likes by a user for the same comment */
+    bool same_entry = false;
+    for (Entity entity : results_likes.asIterable()) {
+      String userId = (String) entity.getProperty("userId");
+      long postId = (long) entity.getProperty("postId");
+      if(userId==userId_like && postId==postId_like){
+        same_entry = true;
+	break;
+      }
+    }
+    if(!same_entry){
+      datastore.put(likesEntity);
+    }
+	
+    Query query_comment = new Query("Comment");
+    PreparedQuery results_comments = datastore.prepare(query_comment);
+	
+    /** Increase the number of likes for a comment by a user for the first time and update in datastore */
+    for (Entity entity : results_comments.asIterable()) {
+      long id = entity.getKey().getId();
+      long likes = (long) entity.getProperty("likes");
+      String userId_comment = (String) entity.getProperty("userId");
+      if(id==postId_like && userId_comment==userId_like && !same_entry)
+        likes = likes + 1;  
+      long createdAt = (long) entity.getProperty("createdAt");
+      String comment = (String) entity.getProperty("comment");
+      String websiteURL = (String) entity.getProperty("websiteURL");
+	  
+      Entity commentEntity = new Entity("Comment", id);
+      commentEntity.setProperty("comment", comment);
+      commentEntity.setProperty("createdAt", createdAt);
+      commentEntity.setProperty("userId", userId_comment);
+      commentEntity.setProperty("likes", likes);
+      commentEntity.setProperty("websiteURL", websiteURL);
+	  
+      datastore.put(commentEntity);
+    }
+    response.sendRedirect("/comment");
+  }
+}
